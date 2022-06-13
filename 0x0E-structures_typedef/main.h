b3a21ffe7009078a2bf60ee3eed2390be81d255b
Minteabi1 <mintesnottsegaye40@gmail.com>
2022-06-13 07:56:34 -0700

    task 6

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,5 @@
+#ifndef _main_h_
+#define _main_h_
+int _putchar(char);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef _main_h_
+#define _main_h_
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
