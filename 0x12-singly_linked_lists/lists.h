782cdbdac0472748e9f2870a20424d1d6900b7fd
Minteabi1 <mintesnottsegaye40@gmail.com>
2022-06-13 07:56:34 -0700

    task 1

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,13 @@
+#ifndef _main_h_
+
+
+#define _main_h_
+
+
+
+
+int _putchar(char);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+size_t list_len(const list_t *h);
+#endif
